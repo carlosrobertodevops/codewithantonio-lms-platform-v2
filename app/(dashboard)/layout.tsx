42177a1aae8ee7e402dcd16a70ca9c2285622eb9
Cyrill Galkin <cyrillgalkin@gmail.com>
2023-12-09 19:55:00 +0300

    Добавил обертку для компонента <Sidebar/>, которая будет скрывать его на мобильном разрешении

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,5 +1,10 @@
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div className="h-full">{children}</div>;
+  return (
+    <div className="h-full">
+      <div className="hidden md:flex h-full w-56 fixed z-50"></div>
+      {children}
+    </div>
+  );
 };
 
 export default DashboardLayout;
