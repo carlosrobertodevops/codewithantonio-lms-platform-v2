6f4794913c3d7b2ecc60583128d107e977939a7c
Cyrill Galkin <cyrillgalkin@gmail.com>
2023-12-10 02:41:29 +0300

    Добавил тэг `<div/>` для стилизации активного роута, который подчеркивает его с правой стороны

--- app/(dashboard)/_components/sidebar-route.tsx
+++ app/(dashboard)/_components/sidebar-route.tsx
@@ -37,6 +37,12 @@ const SidebarRoute = ({ icon: Icon, label, href }: SidebarRouteProps) => {
         />
         {label}
       </div>
+      <div
+        className={cn(
+          'ml-auto opacity-0 border-2 border-sky-700 h-full transition-all',
+          isActive && 'opacity-100',
+        )}
+      />
     </button>
   );
 };
