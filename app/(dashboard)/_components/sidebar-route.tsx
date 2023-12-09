67b2cacf111eaa98cb5d2d4c691d2a343713a978
Cyrill Galkin <cyrillgalkin@gmail.com>
2023-12-10 02:14:12 +0300

    Добавил стили для тега `button`

--- app/(dashboard)/_components/sidebar-route.tsx
+++ app/(dashboard)/_components/sidebar-route.tsx
@@ -1,5 +1,6 @@
 `use client`;
 
+import { cn } from '@/lib/utils';
 import { LucideIcon } from 'lucide-react';
 import { usePathname, useRouter } from 'next/navigation';
 
@@ -20,8 +21,16 @@ const SidebarRoute = ({ icon: Icon, label, href }: SidebarRouteProps) => {
   };
 
   return (
-    <button onClick={onClick} type="button">
-      Sidebar Route
+    <button
+      onClick={onClick}
+      type="button"
+      className={cn(
+        'flex items-center gap-x-2 text-slate-500 text-sm font-medium pl-6 transition-all hover:text-slate-600 hover:bg-slate-300/20',
+        isActive &&
+          'text-sky-700 bg-sky-200/20 hover:bg-sky-200/20 hover:text-sky-700',
+      )}
+    >
+      {label}
     </button>
   );
 };
