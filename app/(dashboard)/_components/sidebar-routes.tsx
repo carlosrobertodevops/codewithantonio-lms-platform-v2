f21c9c2fdf6c44890a1ca3809a59c52acc209034
Cyrill Galkin <cyrillgalkin@gmail.com>
2023-12-09 22:44:18 +0300

    Присвоил константе GUEST_ROUTES массив объектов с данными по каждому роуту, который доступен пользователю

--- app/(dashboard)/_components/sidebar-routes.tsx
+++ app/(dashboard)/_components/sidebar-routes.tsx
@@ -1,5 +1,20 @@
 'use client';
 
+import { Compass, Layout } from 'lucide-react';
+
+const GUEST_ROUTES = [
+  {
+    icon: Layout,
+    label: 'Dashboard',
+    href: '/',
+  },
+  {
+    icon: Compass,
+    label: 'Browse',
+    href: '/search',
+  },
+];
+
 const SidebarRoutes = () => {
   return <div className="flex flex-col"></div>;
 };
