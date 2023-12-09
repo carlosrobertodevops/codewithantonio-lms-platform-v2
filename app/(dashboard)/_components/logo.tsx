f7bb1d37b8dbd3b45e86a3f13cee72c72014f60f
Cyrill Galkin <cyrillgalkin@gmail.com>
2023-12-09 20:59:50 +0300

    Добавил обертку и компонент <Logo/>

---
+++ app/(dashboard)/_components/logo.tsx
@@ -0,0 +1,5 @@
+const Logo = () => {
+  return <div className="">This is a logo</div>;
+};
+
+export default Logo;
--- app/(dashboard)/_components/sidebar.tsx
+++ app/(dashboard)/_components/sidebar.tsx
@@ -2,7 +2,11 @@ import Logo from './logo';
 
 const Sidebar = () => {
   return (
-    <div className="bg-white border-r overflow-y-auto shadow-sm flex flex-col w-full"></div>
+    <div className="bg-white border-r overflow-y-auto shadow-sm flex flex-col w-full">
+      <div className="p-6">
+        <Logo />
+      </div>
+    </div>
   );
 };
 
