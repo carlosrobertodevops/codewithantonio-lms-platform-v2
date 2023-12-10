3ece2e0bf7f5f99875aa7895dedb13e1dacfeece
Cyrill Galkin <cyrillgalkin@gmail.com>
2023-12-10 07:16:42 +0300

    Fix bugs

--- app/(dashboard)/_components/sidebar.tsx
+++ app/(dashboard)/_components/sidebar.tsx
@@ -3,7 +3,7 @@ import SidebarRoutes from './sidebar-routes';
 
 const Sidebar = () => {
   return (
-    <div className="bg-white border-r overflow-y-auto shadow-sm flex flex-col w-full">
+    <div className="bg-white border-r overflow-y-auto shadow-sm flex flex-col w-full h-full">
       <div className="p-6">
         <Logo />
       </div>
