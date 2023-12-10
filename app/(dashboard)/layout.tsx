d9e871a6ee571de999db99365027c83772c20328
Cyrill Galkin <cyrillgalkin@gmail.com>
2023-12-10 05:25:04 +0300

    Создал в папке `_components` компонент <Navbar/>, с помощью сниппета, и добавил его в <DashboardLayout/>

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,9 +1,12 @@
+import Navbar from './_components/navbar';
 import Sidebar from './_components/sidebar';
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full">
-      <div className="h-[80px] fixed z-50 md:pl-56 w-full"></div>
+      <div className="h-[80px] fixed z-50 md:pl-56 w-full">
+        <Navbar />
+      </div>
       <div className="hidden md:flex h-full w-56 fixed z-50">
         <Sidebar />
       </div>
