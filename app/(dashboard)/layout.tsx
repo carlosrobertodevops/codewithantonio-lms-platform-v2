bbd2b9878ff51005d5c51ccc08438991c364bb69
Cyrill Galkin <cyrillgalkin@gmail.com>
2023-12-10 05:23:18 +0300

    В <DashboardLayout/> добавил обертку для компонента <Navbar/>, которая будет сдвигать его вправо на ширину <Sidebar/> на разрешении `md` и выше

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -3,6 +3,7 @@ import Sidebar from './_components/sidebar';
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full">
+      <div className="h-[80px] fixed z-50 md:pl-56 w-full"></div>
       <div className="hidden md:flex h-full w-56 fixed z-50">
         <Sidebar />
       </div>
