f67892e10f823e5497164af99e62319ccd6383a6
Cyrill Galkin <cyrillgalkin@gmail.com>
2023-12-10 07:13:01 +0300

    Добавил стили для <SheetContent/>, а так же задал значение аттрибуту `side`

--- app/(dashboard)/_components/mobile-sidebar.tsx
+++ app/(dashboard)/_components/mobile-sidebar.tsx
@@ -8,7 +8,7 @@ const MobileSidebar = () => {
       <SheetTrigger className="md:hidden pr-4 hover:opacity-75 transition">
         <Menu />
       </SheetTrigger>
-      <SheetContent>
+      <SheetContent side={'left'} className="p-0 bg-white">
         <Sidebar />
       </SheetContent>
     </Sheet>
