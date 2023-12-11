0d56dc879ffcd089ded86f752680197b22358a85
Cyrill Galkin <cyrillgalkin@gmail.com>
2023-12-11 11:43:07 +0300

    Восстановил использование сервиса Clerk и удалил Prisma, чтобы выложить проект на Versel

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,9 +1,11 @@
+import { UserButton } from '@clerk/nextjs';
 import MobileSidebar from './mobile-sidebar';
 
 const Navbar = () => {
   return (
     <div className="p-4 border-b flex items-center bg-white shadow-sm">
       <MobileSidebar />
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 };
