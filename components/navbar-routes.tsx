97e251abcdb20bf1ed613db3e396e85ab511115c
Cyrill Galkin <cyrillgalkin@gmail.com>
2023-12-12 12:10:43 +0300

    Refactor <NavbarRoutes/> to include conditional rendering of <Button/> component based on the current page
    - usePathname hook from next/navigation is used to get the current pathname.
    
    - isTeacherPage and isCoursePage variables are introduced to determine if the current page is a teacher page or a course page.
    
    - Implement conditional rendering of the <Button/> component to allow exiting course or teacher pages and entering teacher mode based on the user's current page.
    
    - Pass `size` and `variant` props for styling of the <Button/> component.
    
    - Use <Link/> component from next/navigation for redirect.

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,10 +1,32 @@
 'use client';
 
 import { UserButton } from '@clerk/nextjs';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { Button } from './ui/button';
+import { LogOut } from 'lucide-react';
 
 const NavbarRoutes = () => {
+  const pathname = usePathname();
+  const isTeacherPage = pathname?.startsWith('/teacher');
+  const isCoursePage = pathname?.includes('/chapter');
+
   return (
     <div className="ml-auto flex items-center gap-x-2">
+      {isTeacherPage || isCoursePage ? (
+        <Link href={'/'}>
+          <Button size={'sm'} variant={'ghost'}>
+            <LogOut className="mr-2 h-4 w-4" />
+            Exit
+          </Button>
+        </Link>
+      ) : (
+        <Link href={'/teacher/courses'}>
+          <Button size={'sm'} variant={'ghost'}>
+            Teacher mode
+          </Button>
+        </Link>
+      )}
       <UserButton afterSignOutUrl="/" />
     </div>
   );
