3cc7e0979b50ee269c619c4b6da3f1ed38b72d5c
Cyrill Galkin <cyrillgalkin@gmail.com>
2023-12-16 16:40:02 +0300

    Revert "Test commit"
    This reverts commit 7c23d01d45afb3c61210dec28cd0ba228b49c754.

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,14 +1,5 @@
-import { Button } from '@/components/ui/button';
-import Link from 'next/link';
-
 const CoursesPage = () => {
-  return (
-    <div className="p-6">
-      <Link href={'teacher/create'}>
-        <Button>New Course</Button>
-      </Link>
-    </div>
-  );
+  return <div>This is a courses page</div>;
 };
 
 export default CoursesPage;
