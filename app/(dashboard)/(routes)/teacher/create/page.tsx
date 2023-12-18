24b4eb1ef0d405ef61a1e95ee89e08c13d65fe7e
Cyrill Galkin <cyrillgalkin@gmail.com>
2023-12-18 20:15:04 +0300

    Define a validation schema using `Zod` for a form
    - Import all from library `zod` as `z`
    
    - Define a validation schema named `formSchema` using `z.object()`
    
    - Define a `title` field within `formSchema` as a `string` with a minimum length of 1 character, providing a `custom error message` for empty values.

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -1,5 +1,11 @@
 'use client';
 
+import * as z from 'zod';
+
+const formSchema = z.object({
+  title: z.string().min(1, 'Title is required'),
+});
+
 const CreatePage = () => {
   return <div>This is a create page</div>;
 };
