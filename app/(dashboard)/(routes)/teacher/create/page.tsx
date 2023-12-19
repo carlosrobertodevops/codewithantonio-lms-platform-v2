ebca4b40a0064410994bd4c9155a375e9aba1e71
Cyrill Galkin <cyrillgalkin@gmail.com>
2023-12-20 01:11:48 +0300

    Initialize the `useForm` hook
    - Declare a `useForm` hook from `react-hook-form` library and pass in a `formSchema` as the resolver
    
    - Initialize a `useForm` hook  with default values for the `title` field
    
    - Infer the type of the form values directly from the `formSchema`

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -1,12 +1,21 @@
 'use client';
 
+import { useForm } from 'react-hook-form';
 import * as z from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
 
 const formSchema = z.object({
   title: z.string().min(1, 'Title is required'),
 });
 
 const CreatePage = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: '',
+    },
+  });
+
   return <div>This is a create page</div>;
 };
 
