a6dba121193e0d8833e38686f7e5e90e089f4a2b
Cyrill Galkin <cyrillgalkin@gmail.com>
2023-12-25 19:29:49 +0300

    Check for the existence of `userId`
    If `userId` not exist then return a `NextResponse` with a `401` status code and the text `"Unauthorized"`

--- app/api/courses/route.ts
+++ app/api/courses/route.ts
@@ -5,6 +5,10 @@ export async function POST(request: Request) {
   try {
     const { userId } = auth();
     const { title } = await request.json();
+
+    if (!userId) {
+      return new NextResponse('Unauthorized', { status: 401 });
+    }
   } catch (error) {
     console.log('[COURSES]', error);
     return new NextResponse('Internal Error', { status: 500 });
