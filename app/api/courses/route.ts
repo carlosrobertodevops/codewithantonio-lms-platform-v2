3543e9363b359abaa19dd4acde7ebf31f6395199
Cyrill Galkin <cyrillgalkin@gmail.com>
2023-12-25 19:21:14 +0300

    Get values from `Clerk` and `form` to save them in the database
    - Extract `userId` using `auth()` function from `Clerk`
    
    - Extract `title` from the JSON payload of the incoming HTTP request

--- app/api/courses/route.ts
+++ app/api/courses/route.ts
@@ -1,7 +1,10 @@
+import { auth } from '@clerk/nextjs';
 import { NextResponse } from 'next/server';
 
 export async function POST(request: Request) {
   try {
+    const { userId } = auth();
+    const { title } = await request.json();
   } catch (error) {
     console.log('[COURSES]', error);
     return new NextResponse('Internal Error', { status: 500 });
