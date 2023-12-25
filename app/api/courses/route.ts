f8cda958776265dd68586af075794c69cf2e1705
Cyrill Galkin <cyrillgalkin@gmail.com>
2023-12-25 19:41:24 +0300

    Add extracted values to database
    Use `create()` method to insert a new document into the `courses` collection. That method takes an object as its parameter with a `data` property  to include the extracted values.

--- app/api/courses/route.ts
+++ app/api/courses/route.ts
@@ -1,3 +1,4 @@
+import { db } from '@/lib/db';
 import { auth } from '@clerk/nextjs';
 import { NextResponse } from 'next/server';
 
@@ -9,6 +10,13 @@ export async function POST(request: Request) {
     if (!userId) {
       return new NextResponse('Unauthorized', { status: 401 });
     }
+
+    const course = await db.course.create({
+      data: {
+        userId,
+        title,
+      },
+    });
   } catch (error) {
     console.log('[COURSES]', error);
     return new NextResponse('Internal Error', { status: 500 });
