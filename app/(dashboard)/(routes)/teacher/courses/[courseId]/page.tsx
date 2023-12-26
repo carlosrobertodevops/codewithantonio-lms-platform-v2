4f26bc24ae9fe8a0a2ec148334fc0c3cf88b7b52
Cyrill Galkin <cyrillgalkin@gmail.com>
2023-12-26 09:18:47 +0300

    Create a folder named `[courseId]` and a mock component for handling dynamic routes
    - Create a folder named `[courseId]` for handling dynamic routes using [Dynamic Segment].
    
    - Utilize a snippet to create a mock component for the dynamic route.
    
    - Destructure object `props` to get access to the `params` property and pass the `CourseIdProps` interface.
    
    - Destructure object `params` to get access to the `courseId` property.
    
    - Render value `courseId` in JSX.

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -0,0 +1,12 @@
+interface CourseIdProps {
+  params: {
+    courseId: string;
+  };
+}
+
+const CourseIdPage = ({ params }: CourseIdProps) => {
+  const { courseId } = params;
+  return <div>{courseId}</div>;
+};
+
+export default CourseIdPage;
