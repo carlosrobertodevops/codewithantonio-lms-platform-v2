1cc125c3c856e81dfcb3e0dcab9834e31350c1a1
Cyrill Galkin <cyrillgalkin@gmail.com>
2023-12-27 09:51:07 +0300

    Utilize `cva` utility to add custom styling options
    - Create a variable `backgroundVariants` and `iconVariants` and assign them the value of the `cva` function, which was imported from `class-variance-authority` library.
    
    - The `cva` function takes default styles as the first parameter and an object as the second parameter, allowing us to define custom options and their default values.
    
    - In this case, `backgroundVariants` and `iconVariants` is configured with options for `variant` and `size`, each with their respective default values and styling options.

--- components/icon-badge.tsx
+++ components/icon-badge.tsx
@@ -1,3 +1,42 @@
+import { cva } from 'class-variance-authority';
+
+const backgroundVariants = cva(
+  'rounded-full flex items-center justify-center',
+  {
+    variants: {
+      variant: {
+        default: 'bg-sky-100',
+        success: 'bg-emerald-100',
+      },
+      size: {
+        default: 'p-2',
+        sm: 'p-1',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+      size: 'default',
+    },
+  },
+);
+
+const iconVariants = cva('', {
+  variants: {
+    variant: {
+      default: 'text-sky-700',
+      success: 'text-emerald-700',
+    },
+    size: {
+      default: 'h-8 w-8',
+      sm: 'h-4 w-4',
+    },
+  },
+  defaultVariants: {
+    variant: 'default',
+    size: 'default',
+  },
+});
+
 const IconBadge = () => {
   return <div>This is an Icon Badge</div>;
 };
