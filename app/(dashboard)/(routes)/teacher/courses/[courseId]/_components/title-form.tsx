9a9c6e4ecdb497b188f17c06e3b99775113fe1eb
Cyrill Galkin <cyrillgalkin@gmail.com>
2023-12-28 10:33:41 +0300

    Implement `formSchema` for `<TitleForm/>` Component
    - Import `z` from `zod`
    
    - Create `formSchema` with `title` property in object with validation options and locate it outside the component
    
    - Define validation rules for `title` input field. It must be a `string` with `min` 1 character and has a custom message `Title is required`

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/title-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/title-form.tsx
@@ -1,11 +1,17 @@
 'use client';
 
+import { z } from 'zod';
+
 interface TitleFormProps {
   initialData: {
     title: string;
   };
   courseId: string;
 }
+
+const titleSchema = z.object({
+  title: z.string().min(1, 'Title is required'),
+});
 
 const TitleForm = ({ initialData, courseId }: TitleFormProps) => {
   return <div>This is a title form</div>;
