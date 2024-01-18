9a183d572250af9457337b075aa1a9883dc70363
Cyrill Galkin <cyrillgalkin@gmail.com>
2024-01-18 18:56:14 +0300

    Find the course in database using extracted values and update it
    - Creat the `ContextProps` interface and assigned its value to the second parameter of the `POST` function, where I destructured the `params` property.
    
    - Import the `db` variable to execute database queries using the `Prisma Client`.
    
    - Utilize the `update()` method to modify course information in the `course` collection. This method accepts an object as its parameter, with a `where` property to locate the course and `data` to specify the updated values.
    
    - Use two parameters for searching: the course identifier retrieved from the `params` property and the author's identifier obtained from the `auth` function.
    
    - Utilize the spread operator to pass new course information into the `data` property.

--- app/api/courses/[courseId]/route.ts
+++ app/api/courses/[courseId]/route.ts
@@ -1,7 +1,12 @@
+import { db } from '@/lib/db';
 import { auth } from '@clerk/nextjs';
 import { NextResponse } from 'next/server';
 
-export async function POST(request: Request) {
+interface ContextProps {
+  params: { courseId: string };
+}
+
+export async function POST(request: Request, { params }: ContextProps) {
   try {
     const { userId } = auth();
     const values = await request.json();
@@ -9,6 +14,16 @@ export async function POST(request: Request) {
     if (!userId) {
       return new NextResponse('Unauthorized', { status: 401 });
     }
+
+    const course = await db.course.update({
+      where: {
+        id: params.courseId,
+        userId,
+      },
+      data: {
+        ...values,
+      },
+    });
   } catch (error) {
     console.log('[COURSE_ID]', error);
     return new NextResponse('Internal Error', { status: 500 });
