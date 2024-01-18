f1576845efe5ea07d8b5184198929ceae798615a
Cyrill Galkin <cyrillgalkin@gmail.com>
2024-01-18 18:21:06 +0300

    Retrieve values from `Clerk` and `request` to identify the course requiring an update.
    - Extract `userId` using `auth()` function from `Clerk`
    
    - Get `values` from the JSON payload of the incoming HTTP request

--- app/api/courses/[courseId]/route.ts
+++ app/api/courses/[courseId]/route.ts
@@ -1,7 +1,10 @@
+import { auth } from '@clerk/nextjs';
 import { NextResponse } from 'next/server';
 
 export async function POST(request: Request) {
   try {
+    const { userId } = auth();
+    const values = await request.json();
   } catch (error) {
     console.log('[COURSE_ID]', error);
     return new NextResponse('Internal Error', { status: 500 });
