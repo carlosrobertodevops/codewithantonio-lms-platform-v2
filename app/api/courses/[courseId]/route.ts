451dd0b5df69ded6629587d5f4aaa76009852684
Cyrill Galkin <cyrillgalkin@gmail.com>
2024-01-18 18:23:49 +0300

    Check for the existence of `userId` to avoid typescript error
    If `userId` doesn't exist then return a `NextResponse` with a `401` status code and the text `"Unauthorized"`

--- app/api/courses/[courseId]/route.ts
+++ app/api/courses/[courseId]/route.ts
@@ -5,6 +5,10 @@ export async function POST(request: Request) {
   try {
     const { userId } = auth();
     const values = await request.json();
+
+    if (!userId) {
+      return new NextResponse('Unauthorized', { status: 401 });
+    }
   } catch (error) {
     console.log('[COURSE_ID]', error);
     return new NextResponse('Internal Error', { status: 500 });
