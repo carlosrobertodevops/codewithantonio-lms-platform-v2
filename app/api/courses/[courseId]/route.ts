36b6088039481a792da7fac7a5d85e0018c2eabb
Cyrill Galkin <cyrillgalkin@gmail.com>
2024-01-18 18:16:32 +0300

    Handle server-side error with `NextResponse`
    If an error occurs in the try-catch block, return a `NextResponse` with a `500` status code and the text `"Internal Error"`

--- app/api/courses/[courseId]/route.ts
+++ app/api/courses/[courseId]/route.ts
@@ -1,6 +1,9 @@
+import { NextResponse } from 'next/server';
+
 export async function POST(request: Request) {
   try {
   } catch (error) {
     console.log('[COURSE_ID]', error);
+    return new NextResponse('Internal Error', { status: 500 });
   }
 }
