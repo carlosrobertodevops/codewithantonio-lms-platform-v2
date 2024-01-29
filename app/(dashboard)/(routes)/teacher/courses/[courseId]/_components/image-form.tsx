fbfe10677c6e1b905940193f7893f1f186a39e51
Cyrill Galkin <cyrillgalkin@gmail.com>
2024-01-29 18:24:46 +0300

    Рефакторинг компонента `<ImageForm/>`
    - Заменяю название интерфейса компонента на `ImageFormProps`, где он был создан и присвоен.
    
    - Меняю свойство интерфейса `description` на `imageUrl` и оставляю тип `null`, т.к. данное свойство является опциональным.
    
    - Изменяю название `zod` схемы на `imageFormSchema` и обновляю в ней свойство `description` на `imageUrl`, а так же изменяю его кастомное сообщение.
    
    - Изменяю тип на `ImageFormSchemaType`, внутрь которого передаю обновленную `imageFormSchema`.
    
    - Меняю схему внутри функции `zodResolver`.
    
    - Внутри дженерика хука `useForm` и параметра функции `onSubmit` обновляю тип на `ImageFormSchemaType`.
    
    - Изменяю дефолтные значения `defaultValues` в хуке `useForm`, учитывая возможность того, что свойство `imageUrl` может быть `null`. Для этого использую оператор нулевого слияния `??`, который возвращает значение правого операнда, если значение левого операнда содержит `null` или `undefined`.
    
    - Меняю заголовок компонента на `Course image`.
    
    - Удаляю тег `p` и использую вместо него `React Fragment`, когда компонент находится не в режиме редактирования.
    
    - Удаляю компонент `Form` и все связанные с ним компоненты и использую вместо него `React Fragment`, когда компонент находится в режиме редактирования.

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/image-form.tsx
@@ -0,0 +1,82 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from '@/components/ui/form';
+import { Textarea } from '@/components/ui/textarea';
+import { cn } from '@/lib/utils';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Course } from '@prisma/client';
+import axios from 'axios';
+import { PencilIcon } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import toast from 'react-hot-toast';
+import { z } from 'zod';
+
+interface ImageFormProps {
+  initialData: {
+    imageUrl: string | null;
+  };
+  courseId: string;
+}
+
+const imageFormSchema = z.object({
+  imageUrl: z.string().trim().min(1, 'Image is required'),
+});
+
+type ImageFormSchemaType = z.infer<typeof imageFormSchema>;
+
+const ImageForm = ({ initialData, courseId }: ImageFormProps) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const router = useRouter();
+
+  const form = useForm<ImageFormSchemaType>({
+    mode: 'onBlur',
+    defaultValues: { imageUrl: initialData?.imageUrl ?? '' },
+    resolver: zodResolver(imageFormSchema),
+  });
+
+  const { isValid, isSubmitting } = form.formState;
+
+  const toggleIsEditing = () => setIsEditing((current) => !current);
+
+  const onSubmit = async (values: ImageFormSchemaType) => {
+    try {
+      await axios.patch(`/api/courses/${courseId}`, values);
+      toast.success('Course title updated');
+      toggleIsEditing();
+      router.refresh();
+    } catch {
+      toast.error('Something went wrong');
+    }
+  };
+
+  return (
+    <div className='mt-6 rounded-md border bg-slate-100 p-4'>
+      <div className='flex items-center justify-between font-medium'>
+        Course image
+        <Button variant={'ghost'} onClick={toggleIsEditing}>
+          {isEditing ? (
+            'Cancel'
+          ) : (
+            <>
+              <PencilIcon className='mr-2 h-4 w-4' />
+              Edit
+            </>
+          )}
+        </Button>
+      </div>
+      {!isEditing && <></>}
+      {isEditing && <></>}
+    </div>
+  );
+};
+
+export default ImageForm;
