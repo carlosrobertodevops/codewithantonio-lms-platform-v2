199a6756feff38fb9263d9b18d6eeaa35aa4e234
Cyrill Galkin <cyrillgalkin@gmail.com>
2024-01-29 17:27:44 +0300

    Добавление `UploadDropzone` в `FileUpload`
    Добавил компонент `UploadDropzone` в jsx-разметку и передал в него следующие свойства согласно примеру из [документации](https://docs.uploadthing.com/getting-started/appdir):
    
    		- `endpoint`: Для определения маршрута загрузки и способов обработки файлов на сервере. Его значение мы получаем из объекта `props`.
    
    		- `onClientUploadComplete`: анонимная функция, которая будет вызвана в случае успешной загрузки файла. В качестве параметра она принимает массив с информацией он загруженных файлах, из которого нам необходимо получить значение свойства `url` у первого элемента. Данное значение мы передаем при вызове функции `onChange`, которую получаем из объекта `props`.
    
    		- `onUploadError`: анонимная функция, которая будет вызвана в случае ошибки при загрузке файла на сервер. Если ошибка произойдет, то она выведется в консоль и отобразится компонент `toast` c сообщением `Something went wrong`.

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -2,14 +2,26 @@
 
 import { UploadDropzone } from '@/lib/uploadthing';
 import type { OurFileRouter } from '@/app/api/uploadthing/core';
+import toast from 'react-hot-toast';
 
 interface FileUploadProps {
-  onChange: (url: string) => void;
+  onChange: (url?: string) => void;
   endpoint: keyof OurFileRouter;
 }
 
 const FileUpload = ({ onChange, endpoint }: FileUploadProps) => {
-  return <div></div>;
+  return (
+    <UploadDropzone
+      onClientUploadComplete={(res) => {
+        onChange(res?.[0].url);
+      }}
+      onUploadError={(error: Error) => {
+        toast.error('Something went wrong');
+        console.log(error);
+      }}
+      endpoint={endpoint}
+    />
+  );
 };
 
 export default FileUpload;
