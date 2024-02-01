bd199913960410b556a2a095ecdff5411932cdde
Cyrill Galkin <cyrillgalkin@gmail.com>
2024-02-01 17:50:08 +0300

    Рефакторинг компонента `Combobox`
    - Удалил переменную `frameworks` c массивом опций, который является неизменяемым (захардкоженным).
    
    - Переименовал компонент в `Combobox` и сделал его стрелочной функцией.
    
    - Добавил интерфейс `ComboboxProps` cо следующими свойствами:
    		`options`: Это массив объектов, у которых есть свойства `label` со значением типа `string` и `value` со значением типа `string`. Значения для данного массива мы получаем из коллекции `Category` в базе данных.
    
    		`value`: Это текущее значение контроллируемого элемента типа `string`, которое мы получаем из cвойства `field` компонента `FormField`. Это позволит интегрировать компонент `Form`, в котором используется библиотека `react-hook-form`, c компонентом  `Combobox`.
    
    		`onChange`: Это функция, которую мы получаем из cвойства `field` компонента `FormField`. При каждом вызове она меняет  текущее значение контроллируемого элемента на значение, переданное ей в качестве аргумента.
    
    - Деструктурировал объект пропс, чтобы извлечь необходимые свойства, и присвоил ему значение интерфейса.
    
    - Удалил состояние для переменной `value`, так как данное значение мы будем получать в объекте пропс.
    
    - C помощью множественного выделения изменил переменную `frameworks` и `framework` на `options` и `option` соответственно.
    
    - Изменил фиксированную ширину компонента `Button`, чтобы он растягивался на все свободное пространство.
    
    - В обработчике `onSelect`, который вызывает `callback`, по клику на опцию, добавил функцию, которая меняет текущее значение `value` контроллируемого элемента. В случае, если данная опция уже была выбрана ранее, то значение обнуляется, если нет, то записывается новое.

--- components/ui/combobox.tsx
+++ components/ui/combobox.tsx
@@ -18,32 +18,14 @@ import {
   PopoverTrigger,
 } from '@/components/ui/popover';
 
-const frameworks = [
-  {
-    value: 'next.js',
-    label: 'Next.js',
-  },
-  {
-    value: 'sveltekit',
-    label: 'SvelteKit',
-  },
-  {
-    value: 'nuxt.js',
-    label: 'Nuxt.js',
-  },
-  {
-    value: 'remix',
-    label: 'Remix',
-  },
-  {
-    value: 'astro',
-    label: 'Astro',
-  },
-];
+interface ComboboxProps {
+  options: { label: string; value: string }[];
+  value: string;
+  onChange: (value: string) => void;
+}
 
-const Combobox = () => {
+export const Combobox = ({ options, value, onChange }: ComboboxProps) => {
   const [open, setOpen] = React.useState(false);
-  const [value, setValue] = React.useState('');
 
   return (
     <Popover open={open} onOpenChange={setOpen}>
@@ -52,33 +34,33 @@ const Combobox = () => {
           variant='outline'
           role='combobox'
           aria-expanded={open}
-          className='w-[200px] justify-between'>
+          className='w-full justify-between'>
           {value
-            ? frameworks.find((framework) => framework.value === value)?.label
-            : 'Select framework...'}
+            ? options.find((option) => option.value === value)?.label
+            : 'Select option...'}
           <ChevronsUpDown className='ml-2 h-4 w-4 shrink-0 opacity-50' />
         </Button>
       </PopoverTrigger>
       <PopoverContent className='w-[200px] p-0'>
         <Command>
-          <CommandInput placeholder='Search framework...' />
-          <CommandEmpty>No framework found.</CommandEmpty>
+          <CommandInput placeholder='Search option...' />
+          <CommandEmpty>No option found.</CommandEmpty>
           <CommandGroup>
-            {frameworks.map((framework) => (
+            {options.map((option) => (
               <CommandItem
-                key={framework.value}
-                value={framework.value}
-                onSelect={(currentValue) => {
-                  setValue(currentValue === value ? '' : currentValue);
+                value={option.label}
+                key={option.value}
+                onSelect={() => {
+                  onChange(option.value === value ? '' : option.value);
                   setOpen(false);
                 }}>
                 <Check
                   className={cn(
                     'mr-2 h-4 w-4',
-                    value === framework.value ? 'opacity-100' : 'opacity-0',
+                    option.value === value ? 'opacity-100' : 'opacity-0',
                   )}
                 />
-                {framework.label}
+                {option.label}
               </CommandItem>
             ))}
           </CommandGroup>
@@ -87,5 +69,3 @@ const Combobox = () => {
     </Popover>
   );
 };
-
-export default Combobox;
