89b376f22270015b389e8a8325a4a6ff4a204c65
Cyrill Galkin <cyrillgalkin@gmail.com>
2024-02-03 18:41:20 +0300

    Create a clone of `<DescriptionForm/>` component, rename it to `<PriceForm/>` and render it into `<CourseIdPage/>`
    - Develop a clone component of `description-form.tsx` within the private folder `_components` specifically for components related to the `CourseIdPage`.
    
    - Rename the cloned component and file name to `<PriceForm/>` and `price-form.tsx` accordingly.
    
    - Import and render the newly renamed `<PriceForm/>` component with the same props  into `<CourseIdPage/>`.

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/price-form.tsx
@@ -0,0 +1,118 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from '@/components/ui/form';
+import { Textarea } from '@/components/ui/textarea';
+import { cn } from '@/lib/utils';
+import { zodResolver } from '@hookform/resolvers/zod';
+import axios from 'axios';
+import { PencilIcon } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import toast from 'react-hot-toast';
+import { z } from 'zod';
+
+interface DescriptionFormProps {
+  initialData: {
+    description: string | null;
+  };
+  courseId: string;
+}
+
+const descriptionFormSchema = z.object({
+  description: z.string().trim().min(1, 'Description is required'),
+});
+
+type DescriptionFormSchemaType = z.infer<typeof descriptionFormSchema>;
+
+const PriceForm = ({ initialData, courseId }: DescriptionFormProps) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const router = useRouter();
+
+  const form = useForm<DescriptionFormSchemaType>({
+    mode: 'onBlur',
+    defaultValues: { description: initialData?.description || '' },
+
+    resolver: zodResolver(descriptionFormSchema),
+  });
+
+  const { isValid, isSubmitting } = form.formState;
+
+  const toggleIsEditing = () => setIsEditing((current) => !current);
+
+  const onSubmit = async (values: DescriptionFormSchemaType) => {
+    try {
+      await axios.patch(`/api/courses/${courseId}`, values);
+      toast.success('Course title updated');
+      toggleIsEditing();
+      router.refresh();
+    } catch {
+      toast.error('Something went wrong');
+    }
+  };
+
+  return (
+    <div className='mt-6 rounded-md border bg-slate-100 p-4'>
+      <div className='flex items-center justify-between font-medium'>
+        Course description
+        <Button variant={'ghost'} onClick={toggleIsEditing}>
+          {isEditing ? (
+            'Cancel'
+          ) : (
+            <>
+              <PencilIcon className='mr-2 h-4 w-4' />
+              Edit
+            </>
+          )}
+        </Button>
+      </div>
+      {!isEditing && (
+        <p
+          className={cn(
+            'mt-2 text-sm',
+            !initialData.description && 'italic text-slate-500',
+          )}>
+          {initialData.description ?? 'No description'}
+        </p>
+      )}
+      {isEditing && (
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className='mt-4 space-y-4'>
+            <FormField
+              control={form.control}
+              name='description'
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Textarea
+                      disabled={isSubmitting}
+                      placeholder='e.g. This course is about... '
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className='flex items-center gap-x-2'>
+              <Button disabled={!isValid || isSubmitting} type='submit'>
+                Save
+              </Button>
+            </div>
+          </form>
+        </Form>
+      )}
+    </div>
+  );
+};
+
+export default PriceForm;
