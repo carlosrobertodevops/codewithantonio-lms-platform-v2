a2a2d419e8a98158d18cb356c8dbf6631cca5baa
Cyrill Galkin <cyrillgalkin@gmail.com>
2024-02-06 12:38:01 +0300

    Get values from `Clerk` and `request`
    - Extract `userId` using `auth()` function from `Clerk`
    
    - Extract `url` from the JSON payload of the incoming HTTP request

--- app/api/courses/[courseId]/attachments/route.ts
+++ app/api/courses/[courseId]/attachments/route.ts
@@ -1,9 +1,14 @@
+import { auth } from '@clerk/nextjs';
+import { NextResponse } from 'next/server';
+
 interface ContextProps {
   params: { courseId: string };
 }
 
 export async function POST(request: Request, { params }: ContextProps) {
   try {
+    const { userId } = auth();
+    const { url } = await request.json();
   } catch (error) {
     console.log('[ATTACHMENTS]', error);
     return new NextResponse('Internal Error', { status: 500 });
