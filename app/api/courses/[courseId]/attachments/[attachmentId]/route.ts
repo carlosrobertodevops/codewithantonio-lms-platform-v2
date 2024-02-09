38011c2b1501e3f507010f263d5d6f5defa9654e
Cyrill Galkin <cyrillgalkin@gmail.com>
2024-02-09 10:29:30 +0300

    Удаление записи о вложении из модели `Attachment` в базе данных
    - Создание переменной `attachment`, которой присваивается результат вызова к базе данных.
    
    - Удаление записи в базе данных происходит с помощью асинхронного вызова метода `delete` у модели `attachment`, которая получена с помощью инстанса библиотеки `@prisma/client`, а именно объекта `db`. При вызове метода в объект со свойством `where` передаются следующие даные:
    
    	- `courseId`: Идентификатор курса, которому присвоено данное вложение. Значение данного свойства получено из объекта `params.courseId`.
    
    	- `id`: Идентификатор вложения в модели `Attachment`. Значение данного свойства получено из объекта `params.attachmentId`.
    
    *Удаление записи происходит только в том случае, когда комбинация этих двух параметров совпадает*

--- app/api/courses/[courseId]/attachments/[attachmentId]/route.ts
+++ app/api/courses/[courseId]/attachments/[attachmentId]/route.ts
@@ -24,6 +24,13 @@ export async function DELETE(request: NextRequest, { params }: ContextProps) {
     if (!courseOwner) {
       return new NextResponse('Unauthorized', { status: 401 });
     }
+
+    const attachment = await db.attachment.delete({
+      data: {
+        courseId: params.courseId,
+        attachmentId: params.attachmentId,
+      },
+    });
   } catch (error) {
     console.log('[ATTACHMENTS_ID]', error);
     return new NextResponse('Internal Error', { status: 500 });
