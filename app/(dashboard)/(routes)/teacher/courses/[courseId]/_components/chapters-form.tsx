4ac76ef7b665247ca1a2a040475354a8c6fe7ee9
Cyrill Galkin <cyrillgalkin@gmail.com>
2024-02-11 14:03:28 +0300

    Рефакторинг `ChaptersForm`
    - Изменено название схемы валидации, свойство, которое она валидирует и кастомное сообщение.
    
    - Изменен тип, который был сгенерирован с использованием `utility` функции из библиотеки `zod` и передана новая схема в оператор `typeof.
    
    - Передан новый тип в дженерик хука `useForm`.
    
    - Изменено свойство по умолчанию в объекте конфигурации хука `useForm`, а так же его значение, которое соответствует `''`.
    
    - Передана новая схема в свойство `resolver` объекта конфигурации хука `useForm`.
    
    - C помощью множественного выделения изменён cтейт `isEditing` и все соответствующие ссылки на него, включая функцию `toggleIsCreating()`
    
    - Параметру функции `onSubmit` присвоен новый тип.
    
    - Изменен метод, вызванный у объекта `axios` и значение `URL`, которое передается в качестве аргумента, так как теперь неоходимо создать новую запись в коллекции `Chapter`.
    
    - Изменено сообщение, которое принимает в параметре метод `success` вызванный у объекта `toast` на `Chapter created`.
    
    - Изменен заголовок компонента.
    
    - Изменена иконка и значение компонента `Button`, в случае если режим создания `isCreating` неактивен.
    
    - Удалено условное выражение, которое отображает тег `p` со значением, которое получено в объекте `initialData` или с текстом по умолчанию.
    
    - Изменено свойство `name`  компонента `FormField`, необходимое для регистрации элемента ввода в библиотеке `react-hook-form`.
    
    -  Вместо `TextArea` добавлен компонент `Input` и в него переданы cледующие свойства:
    
    	- `placeholder`: значение, которое будет показывать текст-подсказку.
    
    	- `disabled`: элемент ввода блокируется во время отправки запроса на сервер.
    
    	- `{...field}`: используя спред оператор переданы остальные свойства и методы, полученные в объекте `field`.
    
    - Изменено значение компонента `Button`, который отвечал за отправку значений формы на сервер.
    
    - Удалены лишние импорты.
    
    - Добавлено тернарное выражение, которое отображает стилизованный тег `p` со значением `No chapters` в случае, если режим создания НЕАКТИВЕН и массив `chapters` из объекта `initialData`, является пустым, иначе отображается `React.Fragment`

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
@@ -8,12 +8,12 @@ import {
   FormItem,
   FormMessage,
 } from '@/components/ui/form';
-import { Textarea } from '@/components/ui/textarea';
+import { Input } from '@/components/ui/input';
 import { cn } from '@/lib/utils';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Chapter, Course } from '@prisma/client';
 import axios from 'axios';
-import { PencilIcon } from 'lucide-react';
+import { PlusCircle } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
@@ -25,32 +25,32 @@ interface ChaptersFormProps {
   courseId: string;
 }
 
-const descriptionFormSchema = z.object({
-  description: z.string().trim().min(1, 'Description is required'),
+const chaptersFormSchema = z.object({
+  chapterTitle: z.string().trim().min(1, 'Chapter title is required'),
 });
 
-type DescriptionFormSchemaType = z.infer<typeof descriptionFormSchema>;
+type ChaptersFormSchemaType = z.infer<typeof chaptersFormSchema>;
 
 const ChaptersForm = ({ initialData, courseId }: ChaptersFormProps) => {
-  const [isEditing, setIsEditing] = useState(false);
+  const [isCreating, setIsCreating] = useState(false);
   const router = useRouter();
 
-  const form = useForm<DescriptionFormSchemaType>({
+  const form = useForm<ChaptersFormSchemaType>({
     mode: 'onBlur',
-    defaultValues: { description: initialData?.description || '' },
+    defaultValues: { chapterTitle: '' },
 
-    resolver: zodResolver(descriptionFormSchema),
+    resolver: zodResolver(chaptersFormSchema),
   });
 
   const { isValid, isSubmitting } = form.formState;
 
-  const toggleIsEditing = () => setIsEditing((current) => !current);
+  const toggleIsCreating = () => setIsCreating((current) => !current);
 
-  const onSubmit = async (values: DescriptionFormSchemaType) => {
+  const onSubmit = async (values: ChaptersFormSchemaType) => {
     try {
       await axios.patch(`/api/courses/${courseId}`, values);
-      toast.success('Course title updated');
-      toggleIsEditing();
+      toast.success('Chapter created');
+      toggleIsCreating();
       router.refresh();
     } catch {
       toast.error('Something went wrong');
@@ -60,19 +60,19 @@ const ChaptersForm = ({ initialData, courseId }: ChaptersFormProps) => {
   return (
     <div className='mt-6 rounded-md border bg-slate-100 p-4'>
       <div className='flex items-center justify-between font-medium'>
-        Course description
-        <Button variant={'ghost'} onClick={toggleIsEditing}>
-          {isEditing ? (
+        Course Chapters
+        <Button variant={'ghost'} onClick={toggleIsCreating}>
+          {isCreating ? (
             'Cancel'
           ) : (
             <>
-              <PencilIcon className='mr-2 h-4 w-4' />
-              Edit
+              <PlusCircle className='mr-2 h-4 w-4' />
+              Add
             </>
           )}
         </Button>
       </div>
-      {!isEditing && (
+      {!isCreating && (
         <p
           className={cn(
             'mt-2 text-sm',
@@ -81,20 +81,20 @@ const ChaptersForm = ({ initialData, courseId }: ChaptersFormProps) => {
           {initialData.description ?? 'No description'}
         </p>
       )}
-      {isEditing && (
+      {isCreating && (
         <Form {...form}>
           <form
             onSubmit={form.handleSubmit(onSubmit)}
             className='mt-4 space-y-4'>
             <FormField
               control={form.control}
-              name='description'
+              name='chapterTitle'
               render={({ field }) => (
                 <FormItem>
                   <FormControl>
-                    <Textarea
+                    <Input
                       disabled={isSubmitting}
-                      placeholder='e.g. This course is about... '
+                      placeholder='e.g. Introduction to the course'
                       {...field}
                     />
                   </FormControl>
@@ -104,11 +104,17 @@ const ChaptersForm = ({ initialData, courseId }: ChaptersFormProps) => {
             />
             <div className='flex items-center gap-x-2'>
               <Button disabled={!isValid || isSubmitting} type='submit'>
-                Save
+                Create
               </Button>
             </div>
           </form>
         </Form>
+      )}
+
+      {!isCreating && !initialData.chapters.length ? (
+        <p className='mt-2 text-sm italic text-slate-500'>No chapters</p>
+      ) : (
+        <></>
       )}
     </div>
   );
